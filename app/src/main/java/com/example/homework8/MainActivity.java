702c9a0fd62dda0ef1aea8685deabd5f0af91ae3
yanghaohao <963347149@qq.com>
2020-04-17 08:54:09 +0800

    修改了默认读取文件的位置和方法，并添加了网络权限

--- app/src/main/java/com/example/homework8/MainActivity.java
+++ app/src/main/java/com/example/homework8/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.homework8;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.PixelFormat;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -17,19 +22,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFormat(PixelFormat.TRANSLUCENT);
+        //getWindow().setFormat(PixelFormat.TRANSLUCENT);
         setContentView(R.layout.activity_main);
 
         videoView=(VideoView)findViewById(R.id.video);
         mController = new MediaController(this);
 
-        String videoPath= Environment.getExternalStorageDirectory().getPath()+"/test2.mp4";
-        File video=new File(videoPath);
-        if(video.exists()){
-            videoView.setVideoPath(video.getAbsolutePath());
-            videoView.setMediaController(mController);
-            mController.setMediaPlayer(videoView);
-            videoView.requestFocus();
+        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.INTERNET},0X123);
+    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[]permissions,@NonNull int[] grantResults){
+        if(requestCode==0x123&&grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            Log.d("MainActivity","Ok");
+            Log.d("MainActivity","Ok");
+            File videoFile=new File(getExternalFilesDir("").toString()+"/test3.MP4");
+            if(videoFile.exists()){
+                videoView.setVideoPath(videoFile.getPath());
+                mController.setMediaPlayer(videoView);
+                videoView.setMediaController(mController);
+                videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+                    @Override
+                    public boolean onError(MediaPlayer mp, int what, int extra) {
+                        return false;
+                    }
+                });
+                videoView.requestFocus();
+            }
         }
     }
 }
